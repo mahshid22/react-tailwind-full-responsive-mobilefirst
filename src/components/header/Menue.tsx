7f6269cd39894eefed1fed0924d05de4db48e908
mahshid22 <mahshid.pouyafar@gmail.com>
2024-07-28 18:31:17 +0430

    finish course page

--- src/components/header/Menue.tsx
+++ src/components/header/Menue.tsx
@@ -1,25 +1,54 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+const activeStyle = " bg-[#c9bcbc]";
 
 export default function Menue() {
-    const nav = useNavigate();
+  const nav = useNavigate();
+  const location = useLocation();
 
   return (
     <div className="flex items-center justify-end w-full mt-3 md:justify-between">
       <div className="max-md:hidden flex">
         <img src="/logo.png" alt="" className="w-[45px] h-[45px] rounded-md" />
         <button
-          className="p-3 rounded-md bg-[#c9bcbc] md:mx-2 lg:mx-7"
+          className={`${
+            location.pathname === "/" ? activeStyle : ""
+          } "md:mx-2 p-3 rounded-md border-2"`}
           onClick={() => nav(`/`)}
         >
           Home
         </button>
-        <button className="md:mx-2 lg:mx-7" onClick={() => nav(`/courses`)}>
-          Contact
+        <button
+          className={`${
+            location.pathname === "/courses" ? activeStyle : ""
+          } "md:mx-2 p-3 rounded-md border-2"`}
+          onClick={() => nav(`/courses`)}
+        >
+          Courses
         </button>
-        <button className="md:mx-2 lg:mx-7">About Us</button>
-        <button className="md:mx-2 lg:mx-7">Courses</button>
-        <button className="md:mx-2 lg:mx-7">Pricing</button>
+        <button
+          className={`${
+            location.pathname === "/aboutus" ? activeStyle : ""
+          } "md:mx-2 p-3 rounded-md border-2"`}
+        >
+          About Us
+        </button>
+        <button
+          className={`${
+            location.pathname === "/contacts" ? activeStyle : ""
+          } "md:mx-2 p-3 rounded-md border-2"`}
+        >
+          contacts
+        </button>
+        <button
+          className={`${
+            location.pathname === "/aboutus" ? activeStyle : ""
+          } "md:mx-2 p-3 rounded-md border-2"`}
+        >
+          Pricing
+        </button>
       </div>
       <div className="">
         <button className="bg-white py-4 px-5 rounded-md border-2 w-[99px]">
