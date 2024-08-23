9dd02d66cc8860d9f4b7ece23209469f91100644
mahshid22 <mahshid.pouyafar@gmail.com>
2024-08-23 18:43:10 +0430

    set PWA config in porject

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import {
   BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
 } from "react-router-dom";
-import Home from "./pages/Home";
-import Courses from "./pages/Courses";
 import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/Courses",
-    element: <Courses />,
-  },
-]);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
